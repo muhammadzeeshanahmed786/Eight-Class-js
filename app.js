fb399cc64c5c0bde9d9a1d53eed6c50664a175f0
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-07 00:28:26 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,127 @@
+
+
+
+// var pro=prompt("Enter Your Name")
+// var pro1=+prompt("How many number are print");
+
+// for(var i=0; i<pro1; i++){
+//     console.log(pro,i);
+// }
+
+
+
+
+
+
+
+// var name=prompt("Enter Your Name")
+// var pro=+prompt("Enter Start Number")
+// var pro1=+prompt("Enter last Number");
+
+// for(var i=pro; i<=pro1; i++){
+//     console.log(name,i);
+// }
+
+
+
+// var pro=+prompt("Enter Start Number")
+// var pro1=+prompt("Enter last Number");
+// var name1=+prompt("How many lapces?")
+
+// for(var i=pro; i<=pro1; i=i+ name1){
+//     console.log(i);
+// }
+
+
+
+
+
+
+// var fruits=["apple","banana","pineapple","geapes"]
+// var fruitslength=fruits.length;
+
+// for(var i=0; i<fruitslength; i++){
+//     console.log(fruits[i]);
+// }
+
+
+
+
+
+
+
+
+// var cities=["Karachi","lahore","quetta","peshawar","islamabad"]
+
+
+// var cityNameCheck=prompt("where do you live?");
+
+
+// var cleanName=cities.indexOf(cityNameCheck);
+
+
+// if(cleanName >=0){
+//     console.log("You Live In Clean City")
+// }
+
+// else{
+//     console.log("You dont Live  in a Clean city")
+// }
+
+
+
+
+
+
+
+// var cities=["Karachi","Lahore","Quetta","Peshawar","Islamabad"]
+
+// var userCheckCity=prompt("Enter City Name");
+// var isClean=false;
+
+// for(var i=0; i<cities.length; i++){
+//     if(userCheckCity == cities[i]){
+//         isClean=true
+//     }
+// }
+
+// if(isClean){
+//     console.log("You Live in a Clean City");
+// }
+// else{
+//     console.log("You dont Live in a Clean City");
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+var cities=["Karachi","Lahore","Quetta","Peshawar","Islamabad"]
+
+var userCheckCity=prompt("Enter City Name");
+
+var userCheckCityName=cities.indexOf(userCheckCity);
+
+if(userCheckCityName>=0){
+    console.log("You Live in a Clean City");
+
+}
+
+
+else{
+    console.log("You dont Live in a Clean City");
+
+}
+
+
+
+
+
+
